2e107e9816ebdf252733763aad1c4e99a871f2d7
wildninjas <66208908+wildninjas@users.noreply.github.com>
2020-06-20 12:25:10 -0300

    COm exit

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -2,46 +2,39 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import Container from './styles'
-import RestaurantIcon from '@material-ui/icons/Restaurant';
-import RestaurantMenuIcon from '@material-ui/icons/RestaurantMenu';
-import PhotoCameraIcon from '@material-ui/icons/PhotoCamera';
-import MenuBookIcon from '@material-ui/icons/MenuBook';
-import RoomServiceIcon from '@material-ui/icons/RoomService';
-import PauseCircleFilledIcon from '@material-ui/icons/PauseCircleFilled';
 
 function SideBar ({ page, changePage }) {
   return (
     <Container page={page}>
-      <li 
+      <li
         className={page === 'Orders' ? 'active' : ''}
-        onClick={() => changePage('Orders')} 
+        onClick={() => changePage('Orders')}
       >
-       <RoomServiceIcon style={{ fontSize: 40 }} /><br/> Pedidos
+        Pedidos
       </li>
       <li
         className={page === 'Categories' ? 'active' : ''}
-        onClick={() => changePage('Categories')} 
+        onClick={() => changePage('Categories')}
       >
-        <MenuBookIcon style={{ fontSize: 40 }} /><br/>Categorias
+        Categorias
       </li>
-     
+      <li
+        className={page === 'Sizes' ? 'active' : ''}
+        onClick={() => changePage('Sizes')}
+      >
+        Tamanhos
+      </li>
       <li
         className={page === 'Products' ? 'active' : ''}
         onClick={() => changePage('Products')}
       >
-       <RestaurantIcon style={{ fontSize: 40 }} /><br/> Produtos
-      </li>
-	  <li
-        className={page === 'Suspensos' ? 'active' : ''}
-        onClick={() => changePage('Suspensos')}
-      >
-       <PauseCircleFilledIcon style={{ fontSize: 40 }} /><br/> Ativar/Suspender Produtos
+        Produtos
       </li>
       <li
         className={page === 'Images' ? 'active' : ''}
         onClick={() => changePage('Images')}
       >
-       <PhotoCameraIcon style={{ fontSize: 40 }} /><br/> Imagens
+        Imagens
       </li>
     </Container>
   )
